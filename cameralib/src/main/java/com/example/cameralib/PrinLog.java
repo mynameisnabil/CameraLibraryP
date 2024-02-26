05185394e2db9806959a39d4e1b88490677d9fe2
nabil <mynameisnabil09@gmail.com>
2024-02-26 15:22:53 +0700

    Initial commit

---
+++ cameralib/src/main/java/com/example/cameralib/PrinLog.java
@@ -0,0 +1,12 @@
+package com.example.cameralib;
+
+public class PrinLog {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printLog(String tag, String message) {
+        System.out.println(tag + " : " + message);
+    }
+
+}
